375e6c6b479ca8456d700154f15216910ca13b25
LoayIssa <issaloay680@gmail.com>
2021-05-15 15:58:07 +0300

    getAllArticles

--- main.js
+++ main.js
@@ -4,6 +4,37 @@ const app = express();
 const port = 5000;
 app.use(express.json());
 
+const articles = [
+    {
+    id: 1,
+    title: 'How I learn coding?',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    {
+    id: 2,
+    title: 'Coding Best Practices',
+    description:
+    'Lorem, ipsum dolor sit, Quam, mollitia.',
+    author: 'Besslan',
+    },
+    {
+    id: 3,
+    title: 'Debugging',
+    description:
+    'Lorem, Quam, mollitia.',
+    author: 'Jouza',
+    },
+    ];
+
+    const getAllArticles=   (req,res)=>{
+        res.status(200)
+        res.json(articles)
+    }
+
+  app.get("/articles",getAllArticles)
+
 
 
 
