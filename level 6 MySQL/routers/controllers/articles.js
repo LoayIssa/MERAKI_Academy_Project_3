70c858964084d0235daa2e79ae0715d3bd2ae7cc
LoayIssa <issaloay680@gmail.com>
2021-06-15 23:06:26 +0300

    done ticket from 0 to 7

---
+++ level 6 MySQL/routers/controllers/articles.js
@@ -0,0 +1,102 @@
+const db = require("../../db/db");
+
+const getAllArticles = (req, res) => {
+	const query=`SELECT * FROM articles WHERE is_deleted=0 `;
+	db.query(query,(err,result)=>{
+		if(err) throw err;
+		res.status(200).json(result)
+	});
+
+	
+};
+
+const getArticlesByAuthor = (req, res) => {
+	const author_id = req.query.author;
+	const query = `SELECT * FROM articles WHERE author_id=? AND is_deleted =0;`;
+	const data = [author_id];
+	db.query(query,data,(err,result)=>{
+		if(err) throw err;
+		res.status(200).json(result)
+		console.log("result",result);
+	});
+
+	
+};
+
+const getAnArticleById = (req, res) => {
+	const _id = req.params.id;
+	const query=`SELECT * FROM articles WHERE _id=? `;
+	const data = [_id];
+	db.query(query,(err,result)=>{
+		if(err) throw err;
+		res.status(200).json(result)
+	});
+	
+};
+
+const createNewArticle = (req, res) => {
+	const { title, description, author_id } = req.body;
+    const query =`INSERT INTO articles (title, description, author_id) VALUES (?,?,?)  `;
+	const data =[title, description, author_id];
+	db.query(query,data,(err,result)=>{
+	if(err) throw err
+		res.status(201).json(result)
+		
+	})
+	
+};
+
+const updateAnArticleById = (req, res) => {
+	const id = req.params.id;
+
+	articlesModel
+		.findByIdAndUpdate(id, req.body, { new: true })
+		.then((result) => {
+			res.status(200).json(result);
+		})
+		.catch((err) => {
+			res.send(err);
+		});
+};
+
+const deleteArticleById = (req, res) => {
+	const id = req.params.id;
+
+	articlesModel
+		.findByIdAndDelete(id)
+		.then((result) => {
+			res.status(200).json({
+				success: true,
+				message: `Success Delete atricle with id => ${id}`,
+			});
+		})
+		.catch((err) => {
+			res.send(err);
+		});
+};
+
+const deleteArticlesByAuthor = (req, res) => {
+	const author = req.body.author;
+
+	articlesModel
+		.deleteMany({ author })
+		.then((result) => {
+			res.status(200).json({
+				success: true,
+				message: `Success Delete atricle with id => ${author}`,
+			});
+		})
+		.catch((err) => {
+			res.send(err);
+		});
+};
+
+module.exports = {
+	getAllArticles,
+	getArticlesByAuthor,
+	getAnArticleById,
+	createNewArticle,
+	updateAnArticleById,
+	deleteArticleById,
+	deleteArticlesByAuthor,
+};
